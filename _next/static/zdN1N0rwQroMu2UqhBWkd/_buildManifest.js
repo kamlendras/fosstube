6fa65c93b3fd055426a7afdba460e904a3fee296
KSinghofficial <ksinghofficial@proton.me>
2023-09-12 20:09:19 +0000

    Deploying to gh-pages from  @ 2157f2d159d3efd7107bdaeb82a373e4f77e8498 🚀

---
+++ _next/static/zdN1N0rwQroMu2UqhBWkd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-6aec2ce618e2a362.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
