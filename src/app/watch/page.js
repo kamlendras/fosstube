9e5be6f4b2c7c4074542d4d15b738b350326d681
ksingh <ksinghofficial@proton.me>
2023-09-21 02:46:15 +0530

    added subscribe, share, emoji picker, share

--- src/app/watch/page.js
+++ src/app/watch/page.js
@@ -1,14 +1,37 @@
 "use client"
+import { useState } from "react";
 import 'vidstack/styles/defaults.css';
 import 'vidstack/styles/community-skin/video.css';
 import Toolbar from '@mui/material/Toolbar';
 import { MediaCommunitySkin, MediaOutlet, MediaPlayer, MediaPoster } from '@vidstack/react';
 import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
+import EmojiPicker from 'emoji-picker-react';
 export default function App() {
+  const [showMore, setShowMore] = useState(false);
+  const {text} = "knsdknknksjnkvd"
   return (
     <>
     <Toolbar/>
-    <Grid container spacing={2}>
+    <Grid container spacing={0}>
         <Grid item xs={8}>
         <MediaPlayer
     title="Sprite Fight"
@@ -42,9 +65,56 @@ export default function App() {
         <Grid item xs={4}>
          <>hello</>
         </Grid>
+
+        </Grid>
+      
+        Baalam Gunda (Official Video) | Fiza Choudhary, Tushar Goel | New Haryanvi Songs Haryanavi 2023
+      
+       
+
+
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         
-      </Grid>
+        
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="AMOGYA MUSIC" secondary="9.25K subscribers" />
+      </ListItem>
+     
+     
+    </List>
+  
     
+   
+    <Fab variant="extended" size="small" color="white">
+        
+        subscribe
+      </Fab>
+      <Fab variant="extended" size="small">
+        <ThumbUpOutlinedIcon sx={{ mr: 1 }} />
+        900 |  
+
+        <ThumbDownOutlinedIcon sx={{ ml: 1 }}/>
+      </Fab>
+      <Fab variant="extended" size="small">
+        <ShareOutlinedIcon sx={{ mr: 1 }} />
+        share
+      </Fab>
+      <Fab variant="extended" size="small">
+        <FileDownloadOutlinedIcon sx={{ mr: 1 }} />
+        Download
+      </Fab>
+      <Fab  size="small">
+        <MoreHorizOutlinedIcon  />
+      
+      </Fab>
+      <div>
+      <EmojiPicker />
+    </div>
   </>
   );
 }
