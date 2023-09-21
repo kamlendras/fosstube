c90cf4a372545df7aa492d55f464766f310e7739
ksingh <ksinghofficial@proton.me>
2023-09-22 00:01:20 +0530

    updated design of drawer

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,7 +91,7 @@ const Drawer = styled(MuiDrawer, {
 
 export default function RootLayout({ children }) {
   
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
     const toggleDrawer = () => {
       setOpen(!open);
     };
