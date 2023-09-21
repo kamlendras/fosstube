f07d17bd1036ca50d477b1879d6a221db6d5309d
ksingh <ksinghofficial@proton.me>
2023-09-21 22:31:52 +0530

    fixed bun workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,17 @@
-import Head from "next/head";
+import { Metadata } from 'next'
 
 import Toolbar from '@mui/material/Toolbar';
 import styles from "./styles/Favorites.module.css";
 import Cardgrid from './components/home/cardgrid'
 import Chips from './components/chips'
+export const metadata = {
+  title: 'FossTube',
+  description: 'Open & Decentralized',
+}    
 export default function C() {
   return (
     <>
-      <Head>
-        <title>codnap</title>
-        <meta
-          name="description"
-          content="Code The World"
-        />
-        <link rel="icon" href="/codnapwhite.svg" />
-      </Head>
+    
 
     <Toolbar/>
 
