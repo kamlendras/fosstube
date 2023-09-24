e5d04121132e3ad12883051ebbc5024379975ebd
KSingh <ksinghofficial@proton.me>
2023-09-25 00:07:52 +0530

    fixed avatar, integrated next auth to menu
    Co-Authored-By: globalngo <145903442+globalngo@users.noreply.github.com>

--- src/app/components/switchaccount.js
+++ src/app/components/switchaccount.js
@@ -15,12 +15,16 @@ import Brightness3OutlinedIcon from "@mui/icons-material/Brightness3Outlined";
 import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import { signIn, signOut, useSession } from "next-auth/react"
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 import SwitchAccountOutlinedIcon from "@mui/icons-material/SwitchAccountOutlined";
 import KeyboardOutlinedIcon from "@mui/icons-material/KeyboardOutlined";
+import styles from "../styles/header.module.css"
 export default function NestedList() {
+  const { data: session, status } = useSession()
+  const loading = status === "loading"
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
@@ -39,8 +43,61 @@ export default function NestedList() {
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List sx={{ pl: 3 }} component="div" disablePadding>
+
+        <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
+          {!session && (
+            <>
+              <span className={styles.notSignedInText}>
+                You are not signed in
+              </span>
+              <a
+                href={`/api/auth/signin`}
+                className={styles.buttonPrimary}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signIn()
+                }}
+              >
+                Sign in
+              </a>
+            </>
+          )}
+          {session?.user && (
+            <>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+             
+            </>
+          )}
+        </p>
+      
+
+        {session?.user && (
+            <>
+             
+              <a
+                href={`/api/auth/signout`}
+                // className={styles.button}
+              
+              >
           <ListItemButton
-   
+   onClick={(e) => {
+                  e.preventDefault()
+                  signOut()
+                }}
            >
             <ListItemIcon>
               <PersonAddAltIcon />
@@ -48,7 +105,9 @@ export default function NestedList() {
             <ListItemText primary="add account" />
            
           </ListItemButton>
-          
+          </a>
+            </>
+          )}
           
         </List>
       </Collapse>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -357,7 +357,7 @@ export default function RootLayout({
 
 
        
-<SignIn/>
+<Avatar/>
 
 
      
