43971c841850d88a9c3ac3705dda3d3a1141ec7b
KSingh <ksinghofficial@proton.me>
2023-09-26 01:11:06 +0530

    resolved module error
    Co-Authored-By: globalngo <145903442+globalngo@users.noreply.github.com>

--- src/app/explore/page.jsx
+++ src/app/explore/page.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Codecarddata from "../../components/codecarddata";
 
-import styles from "../styles/Explore.module.css";
+import styles from "../../styles/Explore.module.css";
 import { Box, Container, Grid } from '@mui/material';
 export default function Explore() {
 
