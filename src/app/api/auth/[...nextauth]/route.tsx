98d29ddab49a93278f6fde6655fde138e4e034f9
KSingh <ksinghofficial@proton.me>
2023-09-29 23:38:48 +0530

    fixed authjs 1
    Co-Authored-By: globalngo <145903442+globalngo@users.noreply.github.com>

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,5 @@
-import NextAuth from 'next-auth'
-import { options } from './options'
+import NextAuth from "next-auth/next"
+import { config } from "auth"
 
-const handler = NextAuth(options)
-
+const handler = NextAuth(config)
 export { handler as GET, handler as POST }
