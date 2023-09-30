875d744db4211ba3803db9e297403fc93575c2fd
KSingh <ksinghofficial@proton.me>
2023-09-30 23:32:45 +0530

    fixed internal links

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Toolbar from "@mui/material/Toolbar";
-import styles from ".././styles/Favorites.module.css";
-import Cardgrid from ".././components/home/cardgrid";
-import Chips from ".././components/chips";
+import styles from "../styles/Favorites.module.css";
+import Cardgrid from "../components/home/cardgrid";
+import Chips from "../components/chips";
 export const metadata = {
   title: "FossTube",
   description: "Open & Decentralized",
