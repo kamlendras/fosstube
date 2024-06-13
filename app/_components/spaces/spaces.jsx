8fbe14899f70f32d5333fd04963c1705cdfa098f
Kamlendra Singh <kamlendrax@gmail.com>
2024-06-13 22:36:50 +0530

    fix deploy errors (#90)
    * add spaces page
    
    * add redirect to report
    
    * fix deploy errors

--- app/_components/spaces/spaces.jsx
+++ app/_components/spaces/spaces.jsx
@@ -1,81 +1,99 @@
 "use client"
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import AppBar from "@mui/material/AppBar";
+import { Toolbar } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import  { useState } from "react";
-
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
-export default function Album() {
+export default function SizeAvatars() {
   const [active, setActive] = useState(false);
     const handleClick = () => {
       setActive(!active);
     };
   return (
-   <>
-     
-        <Toolbar/>
+    <>
+      <Toolbar />
+      <Toolbar />
 
-       
-      <main>
-     
-      
-        
-       
-        <Container sx={{ py: 2 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      FossTube
-                    </Typography>
-                    <Typography>
-                     FossTube official channel.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={handleClick}>{ active ? "following" : "follow"}</Button>
-                    
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      
-      </>
+<Grid spacing={0}>
 
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        // justifyContent="center"
+      
+      >
+        <Grid xs={2}>
+          <Avatar
+            alt="Remy Sharp"
+            src="/static/images/avatar/1.jpg"
+            sx={{ width: 100, height: 100 }}
+          />
+        </Grid>
+
+        <Grid xs={2}>
+          <Typography variant="body1" gutterBottom>
+            Amanda Pulitano
+          </Typography>
+        </Grid>
+        <Grid xs={2}>
+          <Typography variant="body2" gutterBottom>
+            347K subscribers
+          </Typography>
+        </Grid>
+        <Grid xs={2}>
+          <Button variant="contained"    onClick={handleClick} style={{
+        borderRadius: 35,
+        backgroundColor: "#e53935",
+        color:"white"
+      
+    }}>  { active ? "Subscribed" : "Subscribe"}</Button>
+        </Grid>
+      </Grid>
+
+
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      
+      >
+        <Grid xs={2}>
+          <Avatar
+            alt="Remy Sharp"
+            src="/static/images/avatar/1.jpg"
+            sx={{ width: 100, height: 100 }}
+          />
+        </Grid>
+
+        <Grid xs={2}>
+          <Typography variant="body1" gutterBottom>
+            Amanda Pulitano
+          </Typography>
+        </Grid>
+        <Grid xs={2}>
+          <Typography variant="body2" gutterBottom>
+            347K subscribers
+          </Typography>
+        </Grid>
+        <Grid xs={2}>
+        <Button variant="contained"    onClick={handleClick} style={{
+        borderRadius: 35,
+        backgroundColor: "#e53935",
+        color:"white"
+      
+    }}>  { active ? "Subscribed" : "Subscribe"}</Button>
+        </Grid>
+      </Grid>
+
+      </Grid>
+    </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 "use client";
 import * as React from "react";
-import type { ReactNode } from "react"
-import {useTheme,styled,createTheme,ThemeProvider,} from "@mui/material/styles";
-import Image from 'next/image'
-import { useSession, signIn, signOut } from "next-auth/react";
+import {styled,createTheme,ThemeProvider,} from "@mui/material/styles";
+// import Image from 'next/image'
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -12,7 +10,6 @@ import HomeIcon from "@mui/icons-material/Home";
 import PlayCircleOutlineOutlinedIcon from "@mui/icons-material/PlayCircleOutlineOutlined";
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 import HistoryIcon from "@mui/icons-material/History";
-import BookmarkIcon from "@mui/icons-material/Bookmark";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import RocketOutlinedIcon from "@mui/icons-material/RocketOutlined";
 import RocketLaunchOutlinedIcon from "@mui/icons-material/RocketLaunchOutlined";
@@ -27,16 +24,13 @@ import OutlinedFlagIcon from "@mui/icons-material/OutlinedFlag";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import FeedbackOutlinedIcon from "@mui/icons-material/FeedbackOutlined";
 import BookOutlinedIcon from "@mui/icons-material/BookOutlined";
-import SignIn from "./_components/signin"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Copyright from "./_components/copyrightr";
 import localFont from "next/font/local";
-import { useState, useCallback, useEffect } from "react";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import CssBaseline from "@mui/material/CssBaseline";
-import CodeIcon from "@mui/icons-material/Code";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from "@mui/material/Toolbar";
@@ -44,27 +38,18 @@ import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import Badge from "@mui/material/Badge";
 import Container from "@mui/material/Container";
 import MenuIcon from "@mui/icons-material/Menu";
 import Notifications from "./_components/notifications";
 import Create from "./_components/create";
 import Avatar from "./_components/avatar";
-import TextField from "@mui/material/TextField";
-import SearchIcon from "@mui/icons-material/Search";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
 import SimpleBar from "simplebar-react";
 import "simplebar-react/dist/simplebar.min.css";
-import SplitPane, { Pane } from "react-split-pane";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import Box from "@mui/material/Box";
-import Appicon from "./_components/appicon";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import "./globals.css";
-import { Inter, Roboto } from "next/font/google";
-import stylesm from "./_styles/msearch.module.scss";
+import { Roboto } from "next/font/google";
 import SessionProvider from "./_components/SessionProvider";
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
@@ -121,31 +106,31 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 
 
 const myFont = localFont({ src: "./_gothic.otf" });
-const useMediaQuery = (width) => {
-  const [targetReached, setTargetReached] = useState(false);
+// const useMediaQuery = (width) => {
+//   const [targetReached, setTargetReached] = useState(false);
 
-  const updateTarget = useCallback((e) => {
-    if (e.matches) {
-      setTargetReached(true);
-    } else {
-      setTargetReached(false);
-    }
-  }, []);
+//   const updateTarget = useCallback((e) => {
+//     if (e.matches) {
+//       setTargetReached(true);
+//     } else {
+//       setTargetReached(false);
+//     }
+//   }, []);
 
-  useEffect(() => {
-    const media = window.matchMedia(`(max-width: ${width}px)`);
-    media.addListener(updateTarget);
+//   useEffect(() => {
+//     const media = window.matchMedia(`(max-width: ${width}px)`);
+//     media.addListener(updateTarget);
 
     // Check on mount (callback is not called until a change occurs)
-    if (media.matches) {
-      setTargetReached(true);
-    }
+//     if (media.matches) {
+//       setTargetReached(true);
+//     }
 
-    return () => media.removeListener(updateTarget);
-  }, [updateTarget, width]);
+//     return () => media.removeListener(updateTarget);
+//   }, [updateTarget, width]);
 
-  return targetReached;
-};
+//   return targetReached;
+// };
 export default function RootLayout({
   children,
 }: {
@@ -154,7 +139,7 @@ export default function RootLayout({
 
   // const { data: session, status } = useSession()
   // const session = useSession();
-  const loading = status === "loading"
+  // const loading = status === "loading"
   const pathname = usePathname();
 
   const [open, setOpen] = React.useState(true);
@@ -181,7 +166,7 @@ export default function RootLayout({
     [mode],
   );
 
-  const isBreakpoint = useMediaQuery(768);
+  // const isBreakpoint = useMediaQuery(768);
   return (
       <SessionProvider>
     <html lang="en">
@@ -193,107 +178,9 @@ export default function RootLayout({
           >
             <CssBaseline />
 
-            {/* v */}
-            {isBreakpoint ? <></> : <></>}
-
-            {isBreakpoint ? (
-              <>
-                {/* <Mnotifications />  */}
-                <AppBar position="absolute" color="primary">
-                  <Toolbar
-                    variant="dense"
-                    // sx={{
-                    //   pr: "24px",
-                    // }}
-                  >
-                    {/* <span className="menuicon">
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: "10px",
-                // ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-          </span> */}
-                    <Typography
-                      component="h1"
-                      variant="h4"
-                      color="inherit"
-                      noWrap
-                      sx={{ flexGrow: 1 }}
-                    >
-                      <main className={myFont.className}>
-                        <YouTubeIcon sx={{ fontSize: 60, }} style={{fill: "green"}}/>
-                        FossTube
-                      </main>
-                    </Typography>
-
-                    {/* <div className="wrap">
-  <div className="search">
-    <input type="text" className="searchTerm" placeholder="Search FossTube" />
-    <button type="submit" className="searchButton">
-    <SearchOutlinedIcon/>
-    </button>
-  </div>
-</div> */}
-
-                    {/* {theme.palette.mode}
-          <IconButton
-            sx={{ ml: 1 }}
-            onClick={colorMode.toggleColorMode}
-            color="inherit"
-          >
-            {theme.palette.mode === "dark" ? (
-              <Brightness7Icon />
-            ) : (
-              <Brightness4Icon />
-            )}
-          </IconButton> */}
-                    {/* <Create />
-          <Notifications /> */}
-                    <div className={stylesm.container}>
-                      <form 
-                      // action
-                       className={stylesm.search}>
-                        <input
-                          className={stylesm.search__input}
-                          type="search"
-                          placeholder="Search FossTube"
-                          id="searchInput"
-                        />
-                        <div className={stylesm.xp}>
-                          <label
-                            htmlFor="searchInput"
-                            className={stylesm.search__label}
-                            aria-label="Search FossTube"
-                          >
-                            <IconButton>
-                              <SearchOutlinedIcon />
-                            </IconButton>
-                          </label>
-                          <button
-                            className={stylesm.search__submit}
-                            aria-label="Search FossTube"
-                          >
-                            <IconButton>
-                              <SearchOutlinedIcon />
-                            </IconButton>
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-
-                    <Avatar />
-                  </Toolbar>
-                </AppBar>
-              </>
-            ) : (
-              <>
+         
+               
+       
                 <AppBar position="absolute" color="primary">
                   <Toolbar
                     variant="dense"
@@ -373,9 +260,7 @@ export default function RootLayout({
 
                   </Toolbar>
                 </AppBar>
-              </>
-            )}
-            {/* b */}
+        
 
             <span>
               <Drawer variant="permanent" open={open}>
@@ -627,7 +512,7 @@ export default function RootLayout({
                           <Link href="https://ueats.vercel.app/">
                             <ListItemButton>
                               <ListItemIcon>
-                                <Image
+                                <img
                                   src="/ueats.jpeg"
                                   height={26}
                                   width={26}
@@ -656,15 +541,13 @@ export default function RootLayout({
                             </ListItemButton>
                           </Link>
                           <Link
-                            href="/report"
-                            className={pathname == "/report" ? "active" : ""}
+                            href="https://github.com/universal-org/fosstube/issues"
+                           
                           >
                             <ListItemButton>
                               <ListItemIcon>
                                 <OutlinedFlagIcon
-                                  className={
-                                    pathname == "/report" ? "active" : ""
-                                  }
+                               
                                 />
                               </ListItemIcon>
                               <ListItemText primary="Report" />
@@ -686,7 +569,7 @@ export default function RootLayout({
                             </ListItemButton>
                           </Link>
                           <Link
-                            href="https://github.com/fosstube/fosstube.github.io/issues"
+                            href="https://github.com/universal-org/fosstube/issues"
                            
                           >
                             <ListItemButton>
@@ -720,7 +603,7 @@ export default function RootLayout({
             >
               <Container 
               // maxWidth="xl"
-maxWidth="100vw"
+// maxWidth="100vw"
 
 
                sx={{ mt: 0.90 }}
